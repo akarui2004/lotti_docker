5191a567ec91e90ed966916815ba8d618bcabea0
Minh Doan Phan Cong <minhdpc@gmail.com>
2022-11-11 11:10:51 +0700

    chown the www-data to run inside the container

--- app/script.sh
+++ app/script.sh
@@ -3,3 +3,4 @@
 service php${PHP_VERSION}-fpm start
 nginx -g "daemon off;"
 cron -f
+chown --changes --silent --no-dereference --recursive --from=33:33 1000:1000 ~/.config/composer /var/run/php /var/lib/php/sessions
